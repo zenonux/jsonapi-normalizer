fba8512a8a6942ce40bffc0917c62569c6976efc
换个头像心好累 <zenonux@outlook.com>
2022-04-28 09:21:38 +0800

    rm

--- lib/index.ts
+++ lib/index.ts
@@ -47,9 +47,6 @@ function jsonapiToNormItem(
   item: DataIncluedItem,
   included: DataIncluedItem[]
 ): NormalizedDataItem {
-  if (!item) {
-    return {}
-  }
   let newItem = {
     // 注意不要有id冲突的问题
     id: item.id,
@@ -85,11 +82,9 @@ function getRelatedItem(included: DataIncluedItem[], keys: RelatedKey[]) {
   let props: Record<string, any> = {}
   Object.keys(keys).forEach((k) => {
     if (Array.isArray(keys[k].data)) {
+      props[keys[k].property] = []
       keys[k].data.forEach((v: Payloads) => {
         let mapKey = v.type + '_' + v.id
-        if (!props[keys[k].property]) {
-          props[keys[k].property] = []
-        }
         props[keys[k].property].push(map[mapKey])
         setIncluedRelationships(mapKey, map)
       })
@@ -107,8 +102,8 @@ function setIncluedRelationships(mapKey: string, map: Record<string, any>) {
   if (mapValue._relationships) {
     Object.keys(mapValue._relationships).forEach((k) => {
       if (Array.isArray(mapValue._relationships[k])) {
-        mapValue[k]=[]
-        mapValue._relationships[k].forEach(v=>{
+        mapValue[k] = []
+        mapValue._relationships[k].forEach((v) => {
           mapValue[k].push(map[v])
         })
       } else {
@@ -138,7 +133,7 @@ function getIncludedMap(included: DataIncluedItem[]) {
         // 缓存relationships的mapKey
         map[key]._relationships = map[key]._relationships || {}
         if (Array.isArray(v)) {
-            map[key]._relationships[k] = []
+          map[key]._relationships[k] = []
           v.forEach((pv) => {
             map[key]._relationships[k].push(pv.type + '_' + pv.id)
           })
@@ -150,60 +145,3 @@ function getIncludedMap(included: DataIncluedItem[]) {
   }
   return map
 }
-
-// 备选算法，支持多层递归，性能较差
-// function getRelatedItem(included: DataIncluedItem[], keys: RelatedKey[]) {
-//   let props: Record<string, any> = {}
-//   // 已经查找到的属性
-//   let matchedCount = 0
-//   for (let i = 0; i < included.length; i++) {
-//     // 如果已经找到所有关联项，提前结束循环
-//     if (matchedCount >= keys.length) {
-//       return props
-//     }
-//     let item = included[i]
-//     if (
-//       keys.some((v) => {
-//         let keyItem = Array.isArray(v.data) ? v.data[0] : v.data
-//         return keyItem.type == item.type && keyItem.id == item.id
-//       })
-//     ) {
-//       let index = keys.findIndex((v) => {
-//         let keyItem = Array.isArray(v.data) ? v.data[0] : v.data
-//         return keyItem.type == item.type && keyItem.id == item.id
-//       })
-
-//       // 查找的属性为数组
-//       if (Array.isArray(keys[index].data)) {
-//         props[keys[index].property] = props[keys[index].property] || []
-//         props[keys[index].property].push({
-//           id: item.id,
-//           ...item.attributes,
-//         })
-//         if(props[keys[index].property].length >= (keys[index].data as Payloads[]).length){
-//           matchedCount++
-//         }
-//       } else {
-//         matchedCount++
-//         props[keys[index].property] = {
-//           id: item.id,
-//           ...item.attributes,
-//         }
-//         // included包含relationships
-//         if(item.relationships){
-//           let relatedKeys = Object.keys(item.relationships).map((key) => {
-//             return {
-//               property: key,
-//               data: item.relationships[key].data,
-//             }
-//           }).filter(v => !Array.isArray(v.data) || v.data.length > 0)
-//           let newProps = getRelatedItem(included, relatedKeys)
-//           Object.keys(newProps).forEach((v) => {
-//             props[keys[index].property][v] = newProps[v]
-//           })
-//         }
-//       }
-//     }
-//   }
-//   return props
-// }
